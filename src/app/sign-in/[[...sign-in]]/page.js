7b650c8e33d1ded0e5c41f8a6abdf22696fe6b42
Hasnain Siddiqui <146128813+mhasnain23@users.noreply.github.com>
2024-11-12 13:29:11 +0500

    Update page.js

--- src/app/sign-in/[[...sign-in]]/page.js
+++ src/app/sign-in/[[...sign-in]]/page.js
@@ -1,7 +1,7 @@
 import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => {
-  return <SignIn />;
+  return <SignIn className="min-h-screen w-full items-center justify-center"/>;
 };
 
 export default SignInPage;
